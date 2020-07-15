1eecb190956211a1c941c3b5f69665e399efdb42
devrathparadeshi <68229690+devrathparadeshi@users.noreply.github.com>
2020-07-15 19:58:41 +0530

    Add files via upload

---
+++ Assignment_No_02_Day_03.01/Drink.js
@@ -0,0 +1,12 @@
+a=confirm("Is your age 21+  ?");
+
+if (a==true)
+{
+    console.log("Can drink")
+    document.write("<h2>Can drink</h2><br><br><br>")
+}
+else 
+{
+    console.log("Cannot Drink")
+    document.write("<h2>Cannot drink</h2><br><br><br>")
+}
---
+++ Assignment_No_02_Day_03.01/input.js
@@ -0,0 +1,3 @@
+let name = prompt("Enter your name");
+document.write("<h5>Your name is " + name + "<h5>");
+console.log("Your name is " + name  );
